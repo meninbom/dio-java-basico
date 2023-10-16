12242cd04c4d7033cb0c5a0d3b3e61f610b56eef
meninbom <kaua.melo.y@gmail.com>
2023-10-15 21:39:41 -0300

    Operadores Archive

---
+++ java-tipos-e-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        
+        int a,b;
+
+        a = 5;
+        b = 6;
+
+        boolean resultado = a == b;
+
+        System.out.println(resultado);
+    }
+}
